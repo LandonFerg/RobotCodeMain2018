0ddeccce40a8fa28fcfc435bac07beab98a6e7d0
Landon <landonljf@gmail.com>
2018-02-16 16:11:32 -0500

    Added syd controller classes

---
+++ CController.cpp
@@ -0,0 +1,101 @@
+#include "CController.h"
+#include <iostream> 
+#include <Joystick.h>
+#include <XboxController.h>
+#include <iostream>
+#include <GenericHID.h>
+
+using namespace std;
+
+CController::CController()
+{
+    ControllerOne = new XboxController(1);
+    ControllerTwo = new XboxController(2);
+  //  ControllerPanelOne = new XboxController(3);
+  // ControllerPanelTwo = new XboxController(4);
+    PanelJoysticks[0] = new Joystick(3);
+    PanelJoysticks[1] = new Joystick(4);
+}
+
+void CController::getControllerOne()
+{
+    bStartButtonPressed[controller_One] = ControllerOne->GetStartButtonPressed();
+    bStartButtonReleased[controller_One] = ControllerOne->GetStartButtonReleased();
+    bStartButtonRaw[controller_One] = ControllerOne->GetRawButton(Button_Start);
+    
+    bAButtonRaw[controller_One] = ControllerOne->GetRawButton(Button_A);
+    bBButtonRaw[controller_One] = ControllerOne->GetRawButton(Button_B);
+    bXButtonRaw[controller_One] = ControllerOne->GetRawButton(Button_X);
+    bYButtonRaw[controller_One] = ControllerOne->GetRawButton(Button_Y);
+    
+    bAButtonPressed[controller_One] = ControllerOne->GetAButtonPressed();
+    bBButtonPressed[controller_One] = ControllerOne->GetBButtonPressed();
+    bYButtonPressed[controller_One] = ControllerOne->GetYButtonPressed();
+    bXButtonPressed[controller_One] = ControllerOne->GetXButtonPressed();
+    
+    bAButtonReleased[controller_One] = ControllerOne->GetAButtonReleased();
+    bBButtonReleased[controller_One] = ControllerOne->GetBButtonReleased();
+    bYButtonReleased[controller_One] = ControllerOne->GetYButtonReleased();
+    bXButtonReleased[controller_One] = ControllerOne->GetXButtonReleased();
+    
+    bBumperPressed[leftBumperC1] = ControllerOne->GetBumperPressed(Left_Bumper);
+    bBumperPressed[rightBumperC1] = ControllerOne->GetBumperPressed(Right_Bumper);
+    bBumperReleased[leftBumperC1] = ControllerOne->GetBumperReleased(Left_Bumper);
+    bBumperReleased[rightBumperC1] = ControllerOne->GetBumperReleased(Right_Bumper);
+    
+    dLeftXAxis[controller_One] = ControllerOne->GetX(Left_Stick_X);
+    dLeftYAxis[controller_One] = ControllerOne->GetY(Left_Stick_Y);
+    dRightXAxis[controller_One] = ControllerOne->GetX(Right_Stick_X);
+    dRightYAxis[controller_One] = ControllerOne->GetY(Right_Stick_Y);
+}
+
+void CController::getControllerTwo()
+{
+    bStartButtonPressed[controller_Two] = ControllerTwo->GetStartButtonPressed();
+    bStartButtonReleased[controller_Two] = ControllerTwo->GetStartButtonReleased();
+    bStartButtonRaw[controller_Two] = ControllerTwo->GetRawButton(Button_Start);
+    
+    bAButtonRaw[controller_Two] = ControllerTwo->GetRawButton(Button_A);
+    bBButtonRaw[controller_Two] = ControllerTwo->GetRawButton(Button_B);
+    bXButtonRaw[controller_Two] = ControllerTwo->GetRawButton(Button_X);
+    bYButtonRaw[controller_Two] = ControllerTwo->GetRawButton(Button_Y);
+    
+    bAButtonPressed[controller_Two] = ControllerTwo->GetAButtonPressed();
+    bBButtonPressed[controller_Two] = ControllerTwo->GetBButtonPressed();
+    bYButtonPressed[controller_Two] = ControllerTwo->GetYButtonPressed();
+    bXButtonPressed[controller_Two] = ControllerTwo->GetXButtonPressed();
+    
+    bAButtonReleased[controller_Two] = ControllerTwo->GetAButtonReleased();
+    bBButtonReleased[controller_Two] = ControllerTwo->GetBButtonReleased();
+    bYButtonReleased[controller_Two] = ControllerTwo->GetYButtonReleased();
+    bXButtonReleased[controller_Two] = ControllerTwo->GetXButtonReleased();
+    
+    bBumperPressed[leftBumperC2] = ControllerTwo->GetBumperPressed(Left_Bumper);
+    bBumperPressed[rightBumperC2] = ControllerTwo->GetBumperPressed(Right_Bumper);
+    bBumperReleased[leftBumperC2] = ControllerTwo->GetBumperReleased(Left_Bumper);
+    bBumperReleased[rightBumperC2] = ControllerTwo->GetBumperReleased(Right_Bumper);
+    
+    dLeftXAxis[controller_Two] = ControllerTwo->GetX(Left_Stick_X);
+    dLeftYAxis[controller_Two] = ControllerTwo->GetY(Left_Stick_Y);
+    dRightXAxis[controller_Two] = ControllerTwo->GetX(Right_Stick_X);
+    dRightYAxis[controller_Two] = ControllerTwo->GetY(Right_Stick_Y);
+    
+}
+
+void CController::getPanelButtons()
+{
+    for(int i = 0 ; i < 12; i++) 
+    {
+        bPanelButtons[i] = PanelJoysticks[0]->GetRawButton(i);
+        bPanelButtons[12+i] = PanelJoysticks[1]->GetRawButton(i);
+    }
+}
+
+void CController::Update()
+{
+    getPanelButtons();
+    getControllerOne();
+    getControllerTwo();   
+}
+
+
---
+++ CController.h
@@ -0,0 +1,88 @@
+#ifndef CCONTROLLER_H
+#define CCONTROLLER_H
+//put includes
+#include <Joystick.h>
+#include <XboxController.h>
+#include <iostream>
+#include <GenericHID.h>
+
+#define Button_A 1
+#define Button_B 2
+#define Button_X 3
+#define Button_Y 4
+#define Right_Bumper frc::GenericHID::JoystickHand::kRightHand
+#define Left_Bumper frc::GenericHID::JoystickHand::kLeftHand
+#define Button_Back 7
+#define Button_Start 8
+#define Left_Stick_Button Left_Bumper
+#define Right_Stick_Button Right_Bumper
+#define Left_Stick_X Left_Bumper
+#define Left_Stick_Y Left_Bumper
+#define Right_Stick_X Right_Bumper
+#define Right_Stick_Y Right_Bumper
+#define Left_Trigger Left_Bumper
+#define Right_Trigger Right_Bumper
+#define controller_One 0
+#define controller_Two 1
+#define leftBumperC1 controller_One
+#define rightBumperC1 controller_One
+#define leftBumperC2 controller_Two
+#define rightBumperC2 controller_Two
+
+using namespace std;
+
+//class definition
+class CController
+{
+    private:
+        XboxController * ControllerOne;
+        XboxController * ControllerTwo;
+       /* XboxController * ControllerPanelOne;
+        XboxController * ControllerPanelTwo; */
+        Joystick * PanelJoysticks[2];
+        void getControllerOne();
+        void getControllerTwo();
+        
+        
+        void getPanelButtons(); 
+               
+    public:
+        
+        bool bStartButtonPressed [2];
+        bool bStartButtonReleased [2];
+        bool bStartButtonRaw [2];
+        
+        bool bAButtonRaw [2];
+        bool bBButtonRaw [2];
+        bool bXButtonRaw [2];
+        bool bYButtonRaw [2];
+            
+        bool bPanelButtons[24];
+        
+        bool bAButtonPressed [2];
+        bool bBButtonPressed [2];
+        bool bYButtonPressed [2];
+        bool bXButtonPressed [2];
+        
+        bool bAButtonReleased [2];
+        bool bBButtonReleased [2];
+        bool bYButtonReleased [2];
+        bool bXButtonReleased [2];
+        
+        bool bBumperPressed [4];
+        bool bBumperReleased [4];
+        
+        double dLeftXAxis [2];
+        double dLeftYAxis [2];
+        
+        double dRightXAxis [2];
+        double dRightYAxis [2];
+        
+
+        //interface for babies to use
+        CController();
+        void Update();
+        
+};
+
+#endif
